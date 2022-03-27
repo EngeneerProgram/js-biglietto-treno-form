558d1ca0219633d6f5755f9644b8004b05116fe9
Rosario Garufi <saro.garufi1@gmail.com>
2022-03-27 16:15:05 +0200

    Final commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -17,6 +17,13 @@ document.getElementById("mybutton").onclick= function(){
         alert("Inserisci i dati correttamente");
     }
 
+    // Implementazione carrozza random
+    let carrozza = Math.floor(Math.random()*10);
+    console.log(carrozza);
+
+    //implementazione codice cp random
+    let codice = Math.floor(Math.random()*100000);
+    console.log(codice);
 
 
     //sezione sconto minorenne
@@ -24,6 +31,8 @@ document.getElementById("mybutton").onclick= function(){
     let eta = document.getElementById("eta").value;
     if(eta > 0  && eta < 18){
         console.log("Sconto minorenne");
+        document.getElementById("carrozza").innerHTML = (carrozza);
+        document.getElementById("codice").innerHTML = (codice);
         let costo_minorenne = km - ((km * 20)/100);
         costo_minorenne = costo_minorenne.toFixed(2);
         console.log("il costo di ogni km per i minorenne è di " + costo_minorenne);
@@ -32,8 +41,12 @@ document.getElementById("mybutton").onclick= function(){
         costo_biglietto_minore=costo_biglietto_minore.toFixed(2);
         document.getElementById("costo").innerHTML=(costo_biglietto_minore + " €");
         
-    }else if (eta > 65){
+    }
+    //sconto anziano
+    else if (eta > 65){
         console.log("sconto anziano");
+        document.getElementById("carrozza").innerHTML = (carrozza);
+        document.getElementById("codice").innerHTML = (codice);
         document.getElementById("sconto").innerHTML=("Sconto anziani");
         let costo_anziano = km - ((km * 40)/100);
         costo_anziano = costo_anziano.toFixed(2);
@@ -41,13 +54,19 @@ document.getElementById("mybutton").onclick= function(){
         costo_biglietto_anziano = costo_anziano * chilometri_da_percorrere;
         costo_biglietto_anziano=costo_biglietto_anziano.toFixed(2);
         document.getElementById("costo").innerHTML=(costo_biglietto_anziano);
-    }else{
+    }
+    //nessuno sconto
+    else if (eta < 65 || eta > 18 ){
         console.log("Nessuno sconto attivo");
+        document.getElementById("carrozza").innerHTML = (carrozza);
+        document.getElementById("codice").innerHTML = (codice);
         document.getElementById("sconto").innerHTML=("Nessuno sconto attivo");
         costo_no_sconto = km * chilometri_da_percorrere;
         costo_no_sconto = costo_no_sconto.toFixed(2);
         console.log(costo_no_sconto);
         document.getElementById("costo").innerHTML=(costo_no_sconto + " €");
+    }else if (eta < 0 || eta ==0 || eta > 100){
+            console.log("errore");
     }
 
 
